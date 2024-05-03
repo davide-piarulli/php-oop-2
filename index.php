a848866c52c8c17f02603c8299935bae9ebc1bc5
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-03 13:47:50 +0100

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/Model/Cani.php';
+require_once __DIR__ . '/Model/Gatti.php';
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="products">
+      <h1>Prodotti</h1>
+      <div class="product">
+
+        <?php
+
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
